2aacc942dab2e9797c789bf5572dca4a50bf7fa2
Rizky Novando Priyadi <rizkynovando@gmail.com>
2024-09-09 23:28:08 +0700

    fix: rule management get lang

---
+++ src/components/Toast.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <section class="fixed bottom-4 right-4">
+    <p>Ini adalah toast</p>
+  </section>
+</template>
+
+<style scoped>
+
+</style>
